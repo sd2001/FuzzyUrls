7677444e6c26dac2f87aba66ad21662018938097
Swarnabha Das <54739463+sd2001@users.noreply.github.com>
2021-03-01 11:18:32 +0530

    Update settings.py

--- Urlshort/settings.py
+++ Urlshort/settings.py
@@ -131,3 +131,5 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') 
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') 
 django_heroku.settings(locals())
+
+# www.davgo.cf
